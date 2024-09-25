11063cbb8873c1d43a466b9a313c4910e68ce68a
ekrem enes tüzün <166535876+ekremeness@users.noreply.github.com>
2024-09-25 17:38:51 +0300

    pythondeğişkenörnek
    python da değişkenler kuralları ve örnek ile açıklanmıştır.

---
+++ degisken.py
@@ -0,0 +1,22 @@
+maasAli = 5000
+maasAhmet = 4000
+vergi = 0.27
+
+
+print(maasAli - (maasAli*vergi))
+print(maasAhmet - (maasAhmet*vergi))
+
+
+# rakam ile başlayamaz 1number gibi
+# aynı değişken iki kere kullanılamaz kullanırsak ilk değeri siler
+# büyük küçük harf duyarlılığı vardır
+
+
+x = 1
+y = 2.3
+
+name = "enes"
+isStudent = True
+
+# x,y,name,isStudent = (1,2.3,"enes",True)
+
---
+++ degiskentanımlamaornek.py
@@ -0,0 +1,16 @@
+customerName = "enes"
+customerSurname = "tüzün"
+customer = customerName + customerSurname
+gender = True #erkek
+customerId = "656565"
+customerBirthyear = "1998"
+customerCıty = "tokat"
+
+print(customer,customerId,customerCıty,customerBirthyear)
+order1 = 110
+order2 = 1100.2
+order3 = 360.90
+
+total = order1 + order2 + order3
+
+print("total :" , total)
